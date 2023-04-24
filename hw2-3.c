6241be86bbb65142e61f15127bb6ae4cd84d97cb
patty <patrick.celee@gmail.com>
2023-04-24 18:42:49 +0800

    .gitignore

--- hw2-3.c
+++ hw2-3.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <limits.h>
-#include <string.h>
+// Maximum Cardinality Bipartite Matching problem
+// Hopcroft-Karp algorithm
 
 typedef struct F{
     int start, end;
@@ -16,16 +15,36 @@ int main(){
     int n, m, f;
     scanf("%d %d %d", &n, &m, &f);
     flight* flights = (flight*)malloc(sizeof(flight) * n);
-
+    
     for (int i=0;i<f;i++){
         scanf("%d %d", &flights[i].start, &flights[i].end);
     }
 
     qsort(flights, f, sizeof(flight), cmpfunc);
     
+    int* up = (int*)malloc(sizeof(int) * f);
+    int* down = (int*)malloc(sizeof(int) * f);
     for (int i=0;i<f;i++){
-        printf("%d %d\n", flights[i].start, flights[i].end);
+        up[i] = flights[i].start;
+        down[i] = flights[i].end;
     }
+
+
+    int count = 0;
+    int front = flights[0].end;
+
+    for (int i=0;i<f;i++){
+        if (flights[i].end < front){
+            ;
+        }
+
+    }
+
+    for (int i=0;i<f;i++)
+        printf("%2d ", up[i]);
+    printf("\n\n");
+    for (int i=0;i<f;i++)
+        printf("%2d ", down[i]);
 
 
 
