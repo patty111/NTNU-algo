f8c1af8e8815ec60110fef2d3f24ac554839ad77
patty <patrick.celee@gmail.com>
2023-04-26 22:45:38 +0800

    .

---
+++ hw2/hw2-3.c
@@ -0,0 +1,69 @@
+/*
+    Problem #483 - Flight Planning
+    # Longest Increasing Subsequence
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct F{
+    int start, end;
+}flight;
+
+// helper function for binary search
+int binarySearch(int* arr, int left, int right, int x) {
+    while (right-left > 1){
+        int mid = left + (right-left)/2;
+        if (arr[mid] >= x)
+            right = mid;
+        else
+            left = mid;
+    }
+    return right;
+}
+
+
+int LIS(flight* arr, int len) {
+    int* dp = (int*)malloc(len * sizeof(int)); // initialize tails array
+    int max_len = 1;
+    dp[0] = arr[0].end;
+
+    for (int i=1; i<len; i++){
+        if (arr[i].end < dp[0])
+            dp[0] = arr[i].end;
+
+        else if (arr[i].end > dp[max_len-1]) {
+            dp[max_len] = arr[i].end;
+            max_len++;
+        }
+        else {
+            int idx = binarySearch(dp, -1, max_len-1, arr[i].end); // find the index of the smallest element in dp that is greater than or equal to arr[i]
+            dp[idx] = arr[i].end; // replace dp[idx] with arr[i]
+        }
+    }
+
+    free(dp);
+    return max_len;
+}
+
+
+int cmpfunc(const void* a, const void* b){
+    if (((flight*)a)->start != ((flight*)b)->start)
+        return ((flight*)a)->start - ((flight*)b)->start;
+    else
+        return ((flight*)b)->end - ((flight*)a)->end;
+}
+
+int main(){
+    int n, m, f;
+    scanf("%d %d %d", &n, &m, &f);
+    flight* flights = (flight*)malloc(sizeof(flight) * f);
+
+    for (int i=0;i<f;i++){
+        scanf("%d %d", &flights[i].start, &flights[i].end);
+    }
+
+    qsort(flights, f, sizeof(flight), cmpfunc);
+    printf("%d\n", LIS(flights, f));
+    return 0;	
+}
