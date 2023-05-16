8660f4613911662ff1c77c0fd7af5aaeb851b607
patty <patrick.celee@gmail.com>
2023-05-17 03:59:23 +0800

    hw2

---
+++ hw3/hw3-2.cpp
@@ -0,0 +1,13 @@
+/*
+    Problem #502 - Budget Cut
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    
+
+
+    return 0;
+}
---
+++ hw3/max_weight.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <algorithm>
+using namespace std;
+
+typedef struct V{
+    int v;
+    int weight;
+}vertex;
+
+vector<int> path_w;
+
+void printGraph(vector<vector<vertex>> graph){
+    for (int i=1; i<graph.size(); i++){
+        cout << i << ": ";
+        for(auto [vertex, weight] :graph[i]){
+            // auto [vertex, weight] = neighbor;
+            cout << "(" << vertex << ", " << weight << ") ";
+        }
+        cout << endl;
+    }
+}
+
+
+void DFS(vector<vector<vertex>> graph, int n, bool*& visited, int v, int min_w, vector<int>& result, int end){
+    visited[v] = true;
+    cout << v << endl;;
+    // cout << min_w << endl;
+
+    for (auto i: graph[v]){
+        if(!visited[i.v]){
+            min_w = min(min_w, i.weight);
+            // result.push_back(min_w);
+            
+            // end of path
+            if (i.v == end){
+                path_w.push_back(min_w);
+                cout << "end: "<< min_w << endl;
+                return;
+            }
+            DFS(graph, n, visited, i.v, min_w, result, end);
+        }
+    }
+}
+
+
+bool compare_vertices(const vertex& a, const vertex& b) {
+    if (a.v == b.v)
+        return a.weight < b.weight;
+    else
+        return a.v < b.v;
+}
+
+
+int main() {
+    int n, m;  // Number of islands and edges
+    int start, end;
+
+    cin >> n >> m;
+    
+    bool* visited = new bool[n];
+    int min_w = INT_MAX;
+    
+    vector<vector<vertex>> graph(n + 1);  // Graph representation
+    vector<int> result;
+
+
+    for (int i=0; i<m; i++){
+        int a, b, w;
+        cin >> a >> b >> w;
+        graph[a].push_back({b, w});
+        graph[b].push_back({a, w});
+    }
+
+    // sort graph
+    for (auto& neighbors : graph)
+        sort(neighbors.begin(), neighbors.end(), compare_vertices);
+
+    cin >> start >> end;
+
+    DFS(graph, n, visited, start, min_w, result, end);
+
+    printGraph(graph);
+    
+    int ans = INT_MIN;
+    for (auto i: path_w)
+        cout << i << " ";
+        // ans = ans < i ? i : ans;
+    // cout << ans << endl;
+    return 0;
+}
